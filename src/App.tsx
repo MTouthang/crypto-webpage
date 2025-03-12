bad458bfddafc2e9247a22212a0b2f1eea90b724
MTouthang <53576865+MTouthang@users.noreply.github.com>
2025-03-12 17:35:54 +0530

    feat: initial setup

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <h1 className="bg-red-300"> Hello react</h1>
+      <button className='btn'>hello</button>
+    </>
+  )
+}
+
+export default App
